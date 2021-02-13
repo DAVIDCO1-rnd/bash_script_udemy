39013c508133e221d37f0b94bf25211f22824b76
dell <david.cohn.82@gmail.com>
2021-02-14 01:39:32 +0200

    updated

---
+++ bash_developments/family.txt
@@ -0,0 +1,10 @@
+1. davidcohn
+2. yoni
+3. caryn
+4. aviva
+5. margie
+6. chaim
+7. david
+8. aviva
+9. David
+10. david and again david. check if they both were changed
--- bash_developments/recursive_replace.sh
+++ bash_developments/recursive_replace.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Recursively search all files with extension .sh in a given folder and for each file, replace all occurances of "bin" with "banana"
 
-directory="/home/dell/Developments/bash_script_udemy/"
-sh_files=`find $directory -type f -name "*.sh"`
+directory="/home/dell/Developments/bash_script_udemy/bash_developments/"
+sh_files=`find $directory -type f -name "*.txt"`
+# renaming_file='renaming.csv'
+# old_names=`awk -F ',' '{print $1}' $renaming_file$`
+# new_names=`awk -F ',' '{print $2}' $renaming_file$`
+
 for file in $sh_files
 do
-	echo $file
+	old_name='david'
+	new_name='norm'
+	sed -i "s:\b$old_name\b:$new_name:g" $file
 done
