b790170daa0e0a743365ab1bbec1cd0f359cb5c6
dell <david.cohn.82@gmail.com>
2021-02-16 00:25:40 +0200

    updated

--- bash_developments/recursive_replace.sh
+++ bash_developments/recursive_replace.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # Recursively search all files with extension .sh in a given folder and for each file, replace all occurances of "bin" with "banana"
 
-directory=$HOME"/Developments/bash_script_udemy/bash_developments/"
-echo $directory
-sh_files=`find $directory -type f -name "*.txt"`
-# renaming_file='renaming.csv'
-# old_names=`awk -F ',' '{print $1}' $renaming_file$`
-# new_names=`awk -F ',' '{print $2}' $renaming_file$`
+directory=$HOME"/Developments/Renaming/"
+files=`find $directory -type f -name "*.txt"`
+renaming_file='renaming.csv'
+let num_of_lines=`awk '{++cnt} END {print cnt}' $renaming_file`-1
+old_names=`awk -F ',' '{print $1,","}' $renaming_file | tail +2`
+new_names=`awk -F ',' '{print $2,","}' $renaming_file | tail +2`
+old_and_new_names=`awk -F ',' '{print $1,",",$2,":"}' $renaming_file | tail +2`
 
-for file in $sh_files
+echo $num_of_lines
+echo $old_names
+echo $new_names
+echo $old_and_new_names
+
+bb=`awk ":" '{print $1,"\n"}' $old_and_new_names`
+echo $bb
+
+for file in $files
 do
+	bb=awk -F ':' '{print $1,"\n"}' $old_and_new_names
+	echo $bb
+	echo $file
 	old_name='david'
 	new_name='norm'
 	sed -i "s:\b$old_name\b:$new_name:g" $file
