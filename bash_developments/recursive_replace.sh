d07d4c9bbeb14ccdf51e2e02112cacdfed41e33b
David <davidco1-rnd@hotmail.com>
2021-02-17 18:40:59 +0200

    updated code and data

--- bash_developments/recursive_replace.sh
+++ bash_developments/recursive_replace.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# Recursively search all files with extension .sh in a given folder and for each file, replace all occurances of "bin" with "banana"
+# Recursively search all files with given extension in a given folder. For each file, this script replaces all occurances of strings in the first column in a given csv file and replaces them with the strings in the second column of the same csv file.
 
-directory=$HOME"/Developments/Renaming/"
-files=`find $directory -type f -name "*.txt"`
+directory=$HOME"/Developments/Renaming/DATA_FOLDER/"
 renaming_file='renaming.csv'
-let num_of_lines=`awk '{++cnt} END {print cnt}' $renaming_file`-1
+extension="txt"
+num_of_files=`find $directory -type f -name "*."$extension | wc -l`
+files=`find $directory -type f -name "*."$extension`
+let num_of_substitutions=`awk '{++cnt} END {print cnt}' $renaming_file`-1
 old_names=`awk -F ',' '{print $1}' $renaming_file | tail +2`
 new_names=`awk -F ',' '{print $2}' $renaming_file | tail +2`
-old_and_new_names=`awk -F ',' '{print $1,",",$2,":"}' $renaming_file | tail +2`
-
 arr_old_names=($old_names)
 arr_new_names=($new_names)
+arr_files=($files)
 
-for file in $files
+for ((i=0; i<$num_of_files; i++))
 do
-	for ((i=0; i<$num_of_lines; i++))
+	current_file=${arr_files[i]}
+	echo $current_file
+	for ((j=0; j<$num_of_substitutions; j++))
 	do
-		current_old_name=${arr_old_names[i]}
-		current_new_name=${arr_new_names[i]}
-		# awk "{ gsub(/\<$current_old_name\>/, $current_new_name); print }" $file
-		sed -i "s:\b$current_old_name\b:$current_new_name:g" $file
+		current_old_name=${arr_old_names[j]}
+		current_new_name=${arr_new_names[j]}
+		# awk "{ gsub(/\<$current_old_name\>/, $current_new_name); print }" $current_file
+		sed -i "s:\b$current_old_name\b:$current_new_name:g" $current_file
 	done
 done
